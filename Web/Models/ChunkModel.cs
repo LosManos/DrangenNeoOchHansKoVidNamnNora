247482fbac3f2b83f54808f1d775fb0cbf9b009e
Ola Fjelddahl <ola.fjelddahl(diespammers)@gmail.com>
2012-07-19 21:08:52 +0200

    The web now works with workers.

---
+++ Web/Models/ChunkModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ChunkModel
+    {
+        public int ID { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime? Stop { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public void Set(int id, DateTime start, DateTime? stop, TimeSpan? duration)
+        {
+            this.ID = id;
+            this.Start = start;
+
+        }
+    }
+}
---
+++ Web/Models/ChunksModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ChunksModel
+    {
+        public IList<ChunkModel> Items { get; set; }
+        public string WorkerName { get; set; }
+
+        public void Set(string workerName, IList<ChunkModel> items)
+        {
+            this.WorkerName = workerName;
+            this.Items = items;
+        }
+    }
+}
